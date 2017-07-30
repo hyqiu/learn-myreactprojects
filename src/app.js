44e9917f24fd9632f7c60ffa57c433a9e4512b19
Henri Qiu <qiu.henri@gmail.com>
2017-07-30 07:30:26 +0200

    State Control Flow

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Header, Button, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  state = {loggedIn: null};
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBHxVMvha5P4QfP2KJan8PxOmSM24IgxFg',
@@ -14,13 +17,36 @@ class App extends Component {
       storageBucket: 'authentication-4eaec.appspot.com',
       messagingSenderId: '949768173815'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />
+    }
   }
 
   render() {
     return(
       <View>
         <Header headerText="Authentification" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
